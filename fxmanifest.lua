e54e752311656abd4daf4b062aa212c4fc6cd8df
Leah <mail@leahuk.com>
2022-01-19 19:12:04 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ fx_version 'cerulean'
 game 'gta5'
 author 'Leah#0001'
 version '1.0.0'
--- versioncheck 'https://raw.githubusercontent.com/Leah-UK/FiveM-Script-Versioning/main/bixbi_collect.lua'
+versioncheck 'https://raw.githubusercontent.com/Leah-UK/bixbi_gather/main/fxmanifest.lua'
 
 shared_scripts {
 	'@es_extended/imports.lua',
