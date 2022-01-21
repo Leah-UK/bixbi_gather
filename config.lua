61b0362777a3fe831565c16d78519852cdae8f85
Leah <mail@leahuk.com>
2022-01-21 09:59:38 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Debug = false
+Config.Debug = false -- Allows live-restarting of the script.
 Config.BlacklistCertainJobs = true -- Uses bixbi_core illegal blacklist. This is good for performance reasons.
                                    -- Your police / medics etc won't have objects spawn in / the loops wont occur on their client.
 -- For the main chunk of config use sv_config.lua
